88f433225c78ccf534ed3080d617567fa48f86cd
Anirudh-Datti <datti.anirudh@gmail.com>
2026-03-03 08:26:58 +0530

    init commit

---
+++ src/test/java/com/anirudh/Noted/NotedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anirudh.Noted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
